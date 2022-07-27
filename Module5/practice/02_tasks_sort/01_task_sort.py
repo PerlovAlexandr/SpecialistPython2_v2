eae8d5fc6d9f38262c36639b0ecb500a089bac18
PerlovAlexandr <109950471+PerlovAlexandr@users.noreply.github.com>
2022-07-27 12:40:32 +0300

    Update 01_task_sort.py

--- Module5/practice/02_tasks_sort/01_task_sort.py
+++ Module5/practice/02_tasks_sort/01_task_sort.py
@@ -1,6 +1,24 @@
-# Сумма с условием
-# Дан список чисел.
-# Найти: сумму элементов списка, больших данного числа a
+import random
+
+def bubble_sort(nums):
+    swapped = True
+    j = 0
+    while swapped:
+        swapped = False
+        j += 1
+        for i in range(len(nums) - j):
+            if nums[i] > nums[i + 1]:
+                # Меняем элементы
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                # Устанавливаем swapped в True для следующей итерации
+                swapped = True
 
 numbers = [2.5, 13.6, 13, -22.4, -12.8, 6.7, 12.8, 21, 4, -22.1]
-a = ...  # Задайте самостоятельно, выбрав произвольное число
+a = random.randint(-100,100)
+sum_num = 0
+for number in numbers:
+    if number > a:
+        sum_num +=number
+
+print(a)
+print(sum_num)
